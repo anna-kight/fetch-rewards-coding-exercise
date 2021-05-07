f2b28ba051ce6615133078bb191e8b8ef307060d
Anna Kight <anna.kight@gmail.com>
2021-05-07 17:56:43 -0600

    Add functionality to fill out the bowls grids with bar numbers

--- react_app_page.py
+++ react_app_page.py
@@ -29,3 +29,31 @@ def click_reset_button(driver):
 
     button = driver.find_element_by_xpath('/html/body/div/div/div[1]/div[4]/button[1]')
     button.click()
+
+
+def get_measure_results(driver):
+    ''' Gets the measure results (field between the bowls)'''
+
+    button = driver.find_element_by_xpath('/html/body/div/div/div[1]/div[2]/button')
+    results = button.text
+
+    return results
+
+
+def fill_bowl_grid_box(driver, grid, box_number, bar_number):
+    ''' 
+        Puts the value of 'bar_number' into 'box_number' on 'gird'
+        
+        Parameters:
+            grid : str 
+                which bowl the bar number should be put in 'left' or 'right'
+            
+            box_number : int
+                box number that the bar number should be put into boxes are numbered right to left top to bottom and are zero indexed 
+
+            bar_number : int
+                the number that will be typed into the field 
+    '''
+
+    cell = driver.find_element_by_id('{}_{}'.format(grid, box_number))
+    cell.send_keys(bar_number)
